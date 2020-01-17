4f2768c7520d22e62d81024dc22815fa8e8c3906
unknown <calvinp003@gmail.com>
2020-01-17 11:39:43 -0800

    write tests for all validations

--- spec/review_spec.rb
+++ spec/review_spec.rb
@@ -5,4 +5,7 @@ describe Review, type: :model do
   it { should validate_presence_of :author }
   it { should validate_presence_of :rating }
   it { should validate_presence_of :content }
+  it { should validate_inclusion_of(:rating).in_array((1..5).to_a) }
+  it { should validate_length_of(:content).is_at_least(50) }
+  it { should validate_length_of(:content).is_at_most(250) }
 end
