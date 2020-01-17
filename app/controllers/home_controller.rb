23fa1bc27a172bae9b0169458498d474fab33de7
unknown <calvinp003@gmail.com>
2020-01-17 13:12:40 -0800

    add most popular product

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @products = Product.most_recent_three
     @usa_products = Product.made_in_usa
+    @popular = Product.most_reviewed
     :index
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,14 @@ class Product < ApplicationRecord
   scope :made_in_usa, -> {where(country: 'The United States')}
   before_save :capitalize
   validates :name,:cost,:country, presence: true
-
+  scope :most_reviewed, -> {where(id: Product.review_most_product_ids.keys[0])}
   def self.most_recent_three
     Product.last(3)
   end
   def capitalize
     self.name = self.name.titleize()
+  end
+  def self.review_most_product_ids
+    Review.group(:product_id).order('count_id DESC').limit(1).count(:id)
   end
 end
